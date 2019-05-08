6a00c43dd35ec53ceb3d094eb8300fb612d2c182
oky-123 <oky123.ia@gmail.com>
2019-05-08 17:44:20 +0900

    Fix

--- sh/install_fonts.sh
+++ sh/install_fonts.sh
@@ -1,5 +1,19 @@
 mkdir -p $HOME/.fonts
+FONTS_DIRECTORY="${HOME}/.fonts"
 
-git clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git $HOME/.fonts/RictyDiminished
+## Clone fonts
+git clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git $FONTS_DIRECTORY/RictyDiminished
 
-fc-cache -vf $HOME/.fonts/
+## Find and Move .ttf, .ttc to root directory
+if cd "$FONTS_DIRECTORY"; then
+  mv `find . -name "*.tt*" -type f` . 2>/dev/null
+
+  ## Remove directories
+  rm -rf `find * -type d`
+
+  ## Install fonts
+  mv -f ./* /Library/Fonts
+
+  ## Remove fonts directory
+  rm -rf $FONTS_DIRECTORY
+fi
