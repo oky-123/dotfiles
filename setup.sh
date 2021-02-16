a2404b067624c186abc1e93923630f4a4c0f4d70
oky-123 <oky123.ia@gmail.com>
2021-02-17 02:36:26 +0900

    Add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
-ln -s
+
+# Create symbolic links
+IFS=$'\n'
+files=(`cat ./files`)
+
+for file in "${files[@]}"; do
+	rm -rf ~/$file
+	ln -fs ~/dotfiles/$file ~/$file
+done
