ba1349658909b14b4c80085edd9851e8aeaafc91
oky-123 <oky123.ia@gmail.com>
2021-02-17 10:59:15 +0900

    Fix sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,30 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
+# HomeBrewがなければインストール
+xcode-select --install > /dev/null
+
+which brew > /dev/null
+if [ $? == 1 ]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# iTerm2
+# brew install --cask iterm2
+
+# NeoVim
+
+# Python
+## neovim(python)
+
+# java
+
+# Docker
+
+# Google Chrome
+
 # Create symbolic links
+OLDIFS=$IFS
 IFS=$'\n'
 files=(`cat ./files`)
 
@@ -9,3 +32,4 @@ for file in "${files[@]}"; do
 	rm -rf ~/$file
 	ln -fs ~/dotfiles/$file ~/$file
 done
+IFS=$OLDIFS
