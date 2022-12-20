cd586bcc4a82e36ac11a1f316e39cfb21beeeb8d
Yuya Okino <oky123.ia@gmail.com>
2022-12-21 00:31:58 +0900

    feat: neogit.lua追加

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -7,3 +7,4 @@ require("neogit").setup {
 -- Keymappings
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<Leader>n", "<cmd>Neogit<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Leader>d", "<cmd>DiffviewOpen<CR>", opts)
