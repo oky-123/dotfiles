7ca482e3bb0a7fbf51507ce659a447eaf644abf9
oky-123 <oky123.ia@gmail.com>
2023-02-12 20:24:49 +0900

    feat: RubyのLSP周りの設定修正・Masonから外す

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,65 +1,73 @@
 -- LSP Server management
+local lspconfig = require('lspconfig')
 require('mason').setup()
 require('mason-lspconfig').setup_handlers({ function(server)
   local opts = {}
   opts.capabilities = require('cmp_nvim_lsp').default_capabilities()
-  local disable_formatting = function(client)
-    client.server_capabilities.document_formatting = false
-  end
 
   -- Lua
   if server == "sumneko_lua" then
     opts.settings = {
-      Lua = { diagnostics = { globals = { "vim" } } }
+        Lua = { diagnostics = { globals = { "vim" } } }
     }
+    -- Vue
   elseif server == "volar" then
-    opts.on_attach = disable_formatting
     opts.filetypes = {
-      'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json',
+        'typescript', 'javascript', 'vue', 'json',
     }
     opts.root_dir = require('lspconfig/util').root_pattern('package.json', 'tsconfig.json', 'jsconfig.json',
-      'vue.config.js', '.git')
-  elseif server == "tsserver" then
-    opts.on_attach = disable_formatting
-  elseif server == "eslint" then
-    opts.on_attach = disable_formatting
-  elseif server == "solargraph" then
-    -- opts.on_attach = disable_formatting
-    opts.root_dir = require('lspconfig/util').root_pattern("Gemfile", ".git")
-    opts.initial_options = {
-      formatting = true,
-    }
-    opts.settings = {
-      solargraph = {
-        diagnostics = true
-      }
-    }
+            'vue.config.js', '.git')
+    -- Ruby
+    -- elseif server == "solargraph" then
+    --   opts.root_dir = require('lspconfig/util').root_pattern("Gemfile", ".git")
+    --   opts.initial_options = {
+    --       formatting = true,
+    --   }
+    --   opts.settings = {
+    --       solargraph = {
+    --           diagnostics = true
+    --       }
+    --   }
+    -- SQL
   elseif server == "sqlls" then
     opts.root_dir = require('lspconfig/util').root_pattern(".git")
+    --
   elseif server == "sql-formatter" then
     opts.filetypes = {
-      'sql'
+        'sql'
     }
   end
 
-  require('lspconfig')[server].setup(opts)
+  lspconfig[server].setup(opts)
 end })
+
+-- Ruby(without Mason)
+lspconfig.solargraph.setup({
+    cmd = { "solargraph", "stdio" },
+    filetypes = { "ruby" },
+    root_dir = require('lspconfig/util').root_pattern("Gemfile", ".git"),
+    settings = {
+        solargraph = {
+            formatting = true,
+            diagnostics = true
+        }
+    }
+})
 
 -- Required packages
 require('mason-lspconfig').setup({
-  ensure_installed = {
-    "sumneko_lua",
-    "rust_analyzer",
-    "solargraph",
-    "dockerls",
-    "volar",
-    "tsserver",
-    "eslint",
-    "html",
-    "cssls",
-    "jsonls",
-    "bashls",
-  }
+    ensure_installed = {
+        "sumneko_lua",
+        "rust_analyzer",
+        "dockerls",
+        "volar",
+        "tsserver",
+        "eslint",
+        "html",
+        "cssls",
+        "jsonls",
+        "bashls",
+    }
 })
 
 -- UI
@@ -67,8 +75,8 @@ require("fidget").setup {}
 
 -- Trouble(Lsp UI for Telescope)
 require("trouble").setup {
-  auto_open = true, -- automatically open the list when you have diagnostics
-  auto_close = false, -- automatically close the list when you have no diagnostics
+    auto_open = true, -- automatically open the list when you have diagnostics
+    auto_close = false, -- automatically close the list when you have no diagnostics
 }
 
 -- Keymappings
@@ -94,3 +102,4 @@ keymap_n('<Leader>r', "<cmd>lua vim.lsp.buf.rename()<CR>")
 keymap_n('<Leader>ca', "<cmd>Lspsaga code_action<CR>")
 keymap_x('<Leader>ca', ":<C-u>Lspsaga range_code_action<CR>")
 keymap_n('gG', "<cmd>TroubleToggle<CR>")
+keymap_n('<Leader>f', "<cmd>lua vim.lsp.buf.format { async = false, timeout = 5000 }<CR>")
