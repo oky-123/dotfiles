87dfd1b2a722f2357f83e26f724e802b29b77523
oky-123 <oky123.ia@gmail.com>
2023-11-24 22:11:59 +0900

    feat: format修正

--- .custom_shells/run_tbls.sh
+++ .custom_shells/run_tbls.sh
@@ -12,10 +12,22 @@ fi
 function run_tbls_on_docker() {
   NAME=test-db-$RANDOM
   ~/.custom_shells/docker_run_db.sh $NAME ${2:-8}
+  # sleep 20
+
+  # mysql pingを利用して起動を待つ
+  for i in `seq 1 30`;
+  do
+    mysql -uroot -ppassword -h127.0.0.1 -P3307 -e "select 1" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      break
+    fi
+    sleep 1
+  done
+
   mysql -uroot -ppassword -h127.0.0.1 -P3307 test < $1
 
   # dsn: postgres://root:password@127.0.0.1:5432/test
-  tbls --dsn mysql://root:password@127.0.0.1:3307/test doc --force
+  tbls --dsn mysql://root:password@127.0.0.1:3307/test doc --force -j -t mermaid
   docker stop $NAME
 }
 
